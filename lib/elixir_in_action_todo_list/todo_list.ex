b8e7475d4dc95337ae19d7977047af1575bb00ab
Nathan Hessler <xmilestegx@gmail.com>
2017-02-06 18:40:12 -0500

    complete 4.2.1

--- lib/elixir_in_action_todo_list/todo_list.ex
+++ lib/elixir_in_action_todo_list/todo_list.ex
@@ -1,11 +1,19 @@
 defmodule ElixirInActionTodoList.TodoList do
-  alias ElixirInActionTodoList.{MultiMap}
+  alias ElixirInActionTodoList.{TodoList}
 
-  def new, do: MultiMap.new
+  defstruct auto_id: 1, entries: Map.new
 
-  def add_entry(todo_list, entry) do
-    MultiMap.add(todo_list, entry.date, entry.title)
+  def new, do: %TodoList{}
+
+  def add_entry(%TodoList{entries: entries, auto_id: auto_id}, entry) do
+    entry = Map.put(entry, :id, auto_id)
+    new_entries = Map.put(entries, auto_id, entry)
+    %TodoList{ entries: new_entries, auto_id: auto_id + 1 }
   end
 
-  def entries(todo_list, date), do: MultiMap.get(todo_list, date)
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn({_, entry}) -> entry.date == date end)
+    |> Enum.map(fn({_, entry}) -> entry end)
+  end
 end
