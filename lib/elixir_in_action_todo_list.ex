7d2f964590470ea71a409b458813da2bc64d347a
Nathan Hessler <xmilestegx@gmail.com>
2017-02-06 17:36:58 -0500

    mix new todo_list app

---
+++ lib/elixir_in_action_todo_list.ex
@@ -0,0 +1,18 @@
+defmodule ElixirInActionTodoList do
+  @moduledoc """
+  Documentation for ElixirInActionTodoList.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirInActionTodoList.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_in_action_todo_list_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirInActionTodoListTest do
+  use ExUnit.Case
+  doctest ElixirInActionTodoList
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
