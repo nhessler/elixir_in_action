012586e5daef7177bf2ab1b1cdf25e7247ef96f8
Nathan Hessler <xmilestegx@gmail.com>
2017-02-12 14:10:36 -0500

    complete 4.2.5

---
+++ lib/elixir_in_action_todo_list/todo_list/csv_importer.ex
@@ -0,0 +1,18 @@
+defmodule ElixirInActionTodoList.TodoList.CsvImporter do
+
+  def import!(filename) do
+    filename
+    |> File.stream!()
+    |> Stream.map(&String.replace(&1, ~r/\n/, ""))
+    |> Stream.map(&(String.split(&1, ",")))
+    |> Stream.map(&build_date_tuple/1)
+    |> Enum.to_list()
+    |> ElixirInActionTodoList.TodoList.new()
+  end
+
+  defp build_date_tuple([date_string, title]) do
+    [y, m, d] = String.split(date_string, "/")
+    %{date: {String.to_integer(y), String.to_integer(m), String.to_integer(d)},
+      title: title }
+  end
+end
