7e6d5585db925ebb0bf1ca2dac094077f99a06ee
Nathan Hessler <xmilestegx@gmail.com>
2017-02-19 15:10:50 -0500

    complete 6.2.8

--- lib/elixir_in_action_todo_list/todo_server.ex
+++ lib/elixir_in_action_todo_list/todo_server.ex
@@ -1,48 +1,49 @@
 defmodule ElixirInActionTodoList.TodoServer do
-  alias ElixirInActionTodoList.{TodoList, ServerProcess}
+  alias ElixirInActionTodoList.{TodoList}
+
+  use GenServer
 
   # Public API
 
   def start do
-    ServerProcess.start(__MODULE__)
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
   end
 
   def add_entry(new_entry) do
-    ServerProcess.cast(__MODULE__, {:add_entry, new_entry})
+    GenServer.cast(__MODULE__, {:add_entry, new_entry})
   end
 
   def update_entry(entry_id, updater_function) do
-    ServerProcess.cast(__MODULE__, {:update_entry, entry_id, updater_function})
+    GenServer.cast(__MODULE__, {:update_entry, entry_id, updater_function})
   end
 
   def delete_entry(entry_id) do
-    ServerProcess.cast(__MODULE__, {:delete_entry, entry_id})
+    GenServer.cast(__MODULE__, {:delete_entry, entry_id})
   end
 
   def entries(date) do
-    ServerProcess.call(__MODULE__, {:entries, date})
+    GenServer.call(__MODULE__, {:entries, date})
   end
 
-  # ServerProcess Callbacks
+  # GenServer Callbacks
 
-
-  def init() do
-    TodoList.new
+  def init(_) do
+    {:ok, TodoList.new}
   end
 
   def handle_cast({:add_entry, new_entry}, todo_list) do
-    TodoList.add_entry(todo_list, new_entry)
+    {:noreply, TodoList.add_entry(todo_list, new_entry)}
   end
 
   def handle_cast({:update_entry, entry_id, updater_function}, todo_list) do
-    TodoList.update_entry(todo_list, entry_id, updater_function)
+    {:noreply, TodoList.update_entry(todo_list, entry_id, updater_function)}
   end
 
   def handle_cast({:delete_entry, entry_id}, todo_list) do
-    TodoList.delete_entry(todo_list, entry_id)
+    {:noreply, TodoList.delete_entry(todo_list, entry_id)}
   end
 
-  def handle_call({:entries, date}, todo_list) do
-    {TodoList.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _, todo_list) do
+    {:reply, TodoList.entries(todo_list, date), todo_list}
   end
 end
