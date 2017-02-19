7be2b0e62e43e5ca6f4c2dcb8de7df03430a0a10
Nathan Hessler <xmilestegx@gmail.com>
2017-02-19 14:23:59 -0500

    complete 6.1.5

--- lib/elixir_in_action_todo_list/todo_server.ex
+++ lib/elixir_in_action_todo_list/todo_server.ex
@@ -1,43 +1,48 @@
 defmodule ElixirInActionTodoList.TodoServer do
-  alias ElixirInActionTodoList.{TodoList}
+  alias ElixirInActionTodoList.{TodoList, ServerProcess}
+
+  # Public API
+
   def start do
-    pid = spawn(fn -> loop(TodoList.new) end)
-    Process.register(pid, __MODULE__)
+    ServerProcess.start(__MODULE__)
   end
 
   def add_entry(new_entry) do
-    send(__MODULE__, {:add_entry, new_entry})
+    ServerProcess.cast(__MODULE__, {:add_entry, new_entry})
   end
 
   def update_entry(entry_id, updater_function) do
-    send(__MODULE__, {:update_entry, entry_id, updater_function})
+    ServerProcess.cast(__MODULE__, {:update_entry, entry_id, updater_function})
   end
 
   def delete_entry(entry_id) do
-    send(__MODULE__, {:delete_entry, entry_id})
+    ServerProcess.cast(__MODULE__, {:delete_entry, entry_id})
   end
 
   def entries(date) do
-    send(__MODULE__, {:entries, self, date})
-    receive do
-      {:todo_entries, entries} -> entries
-      after 5000 -> {:error, :timeout}
-    end
+    ServerProcess.call(__MODULE__, {:entries, date})
   end
 
-  defp loop(todo_list) do
-    new_todo_list = receive do
-      {:add_entry, new_entry} ->
-        TodoList.add_entry(todo_list, new_entry)
-      {:update_entry, entry_id, updater_function} ->
-        TodoList.update_entry(todo_list, entry_id, updater_function)
-      {:delete_entry, entry_id} ->
-        TodoList.delete_entry(todo_list, entry_id)
-      {:entries, caller, date} ->
-        send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
-        todo_list
-    end
+  # ServerProcess Callbacks
 
-    loop(new_todo_list)
+
+  def init() do
+    TodoList.new
+  end
+
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  def handle_cast({:update_entry, entry_id, updater_function}, todo_list) do
+    TodoList.update_entry(todo_list, entry_id, updater_function)
+  end
+
+  def handle_cast({:delete_entry, entry_id}, todo_list) do
+    TodoList.delete_entry(todo_list, entry_id)
+  end
+
+  def handle_call({:entries, date}, todo_list) do
+    {TodoList.entries(todo_list, date), todo_list}
   end
 end
