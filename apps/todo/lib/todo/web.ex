7724a15bb24ceee6e818daeef102bf605b5d565c
Nathan Hessler <xmilestegx@gmail.com>
2017-12-09 16:21:28 -0800

    complete 11.3.3

--- apps/todo/lib/todo/web.ex
+++ apps/todo/lib/todo/web.ex
@@ -1,4 +1,23 @@
 defmodule Todo.Web do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+
+  post "/add_entry" do
+    conn
+    |> Plug.Conn.fetch_query_params
+    |> add_entry
+    |> respond
+  end
+
+  get "/entries" do
+    conn
+    |> Plug.Conn.fetch_query_params
+    |> get_entries
+    |> respond
+  end
 
   def start_server do
     IO.puts "starting #{__MODULE__}"
@@ -6,4 +25,67 @@ defmodule Todo.Web do
   end
 
   def init(_), do: nil
+
+  defp add_entry(conn) do
+    entry = %{
+      date: parse_date(conn.params["date"]),
+      title: conn.params["title"]
+    }
+
+    conn.params["list"]
+    |> String.to_atom
+    |> Todo.Cache.server_process
+    |> Todo.Server.add_entry(entry)
+
+    Plug.Conn.assign(conn, :response, "OK")
+  end
+
+  defp get_entries(conn) do
+    entries =
+      conn.params["list"]
+      |> String.to_atom
+      |> Todo.Cache.server_process
+      |> Todo.Server.entries(parse_date(conn.params["date"]))
+
+    Plug.Conn.assign(conn, :response, build_entries_response(entries))
+  end
+
+  defp respond(conn) do
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, conn.assigns[:response])
+  end
+
+  defp parse_date(date) do
+    {
+      parse_date_unit(date, 0..3),
+      parse_date_unit(date, 4..5),
+      parse_date_unit(date, 6..7)
+    }
+  end
+
+  defp parse_date_unit(date, range) do
+    date
+    |> String.slice(range)
+    |> String.to_integer
+  end
+
+  defp build_entries_response(entries, response \\ [])
+  defp build_entries_response([ entry | rest], response) do
+     build_entries_response(rest, [ build_entry_response(entry) | response])
+  end
+
+  defp build_entries_response([], response) do
+     response
+    |> Enum.reverse
+    |> Enum.join("\n")
+  end
+
+  defp build_entry_response(entry) do
+    "#{build_date_response(entry[:date])}\t#{entry[:title]}"
+  end
+
+  defp build_date_response(date) do
+    "#{elem(date, 0)}-#{elem(date, 1)}-#{elem(date, 2)}"
+  end
 end
